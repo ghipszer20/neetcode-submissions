6c8ca0d1ffb01deb61d448fe7882b0329778d194
Gavin Hipszer <ghipszer@terpmail.umd.edu>
2026-09-05 14:24:16 -0400

    Add: two-integer-sum - submission-1

---
+++ Data Structures & Algorithms/two-integer-sum/submission-1.py
@@ -0,0 +1,28 @@
+class Solution:
+
+    # hashmap: O(n) time, O(n) memory
+    def twoSum(self, nums, target):
+        hshMap = {}
+
+        for i in range(len(nums)):
+            if target - nums[i] in hshMap:
+                return [hshMap[target - nums[i]], i]
+            hshMap[nums[i]] = i
+        
+        return [-1, -1]
+
+    # brute force: O(n^2) time, O(1) memory
+    # def twoSum(self, nums: List[int], target: int) -> List[int]:
+    #     for i in range(len(nums)):
+    #         for j in range(i + 1, len(nums)):
+    #             if nums[i] + nums[j] == target:
+    #                 return [i, j]
+        
+    #     return [-1, -1]
+
+        
+
+                
+
+        
+        
