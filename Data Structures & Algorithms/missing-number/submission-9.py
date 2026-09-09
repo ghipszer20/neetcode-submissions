9153b3e51f9bf9d9159e1c44bfbcf3d4a76ddcf7
Gavin Hipszer <ghipszer@terpmail.umd.edu>
2026-09-08 22:21:41 -0400

    Add: missing-number - submission-9

---
+++ Data Structures & Algorithms/missing-number/submission-9.py
@@ -0,0 +1,36 @@
+class Solution:
+
+    def missingNumber(self, nums: List[int]) -> int:
+        n = len(nums)
+        return ((n * (n + 1)) // 2) - sum(nums)
+
+    # def missingNumber(self, nums: List[int]) -> int:
+    #     return sum(range(len(nums) + 1)) - sum(nums)
+
+    # def missingNumber(self, nums: List[int]) -> int:
+    #     full_range_sum = 0
+
+    #     for i in range(len(nums) + 1):
+    #         full_range_sum += i
+
+    #     return full_range_sum - sum(nums)
+
+
+
+    # def missingNumber(self, nums: List[int]) -> int:
+    #     test = []
+
+    #     for i in range(len(nums) + 1):
+    #         test.append(i)
+
+    #     return sum(test) - sum(nums)
+
+
+    # def missingNumber(self, nums: List[int]) -> int:
+    #     for i in range(len(nums) + 1):
+    #         if i not in nums:
+    #             return i
+        
+    #     return -1
+        
+        
